06f27d6bc74c4a00bc5dce6cbe86d487939cff76
cWilgar <carolinewilgar@gmail.com>
2017-08-20 23:22:47 +0100

    refactor - following tutorial to avoid bubbling handleClick event through several parents - https://facebook.github.io/react/tutorial/tutorial.html

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import TrumpCards from './trumpCards.json';
 import shuffle from 'shuffle-array';
 
 const SERVED_CARDS = serveCards(TrumpCards);
+
 function serveCards(cards) {
   const shuffledCards = shuffle(cards, { 'copy': true });
   const numToServe = Math.floor(cards.length/2);
@@ -10,34 +11,65 @@ function serveCards(cards) {
     shuffledCards.slice(0,numToServe),
     shuffledCards.slice(numToServe)
   ];
-
-} 
+}
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: SERVED_CARDS, //the players current card is the one at index 0 of each hand
+      currPlayer: 0,
+      selectedCategory: '',
+    }
+  };
+
+  handleCategorySelection(category) {
+    // best-player = 0;
+    // for each player i
+    //   if category rank < player-1's rank
+    //   bestplayer = i
+    // endfor
+
+    // const cards = this.state.cards.slice();
+    // let cardsForTheTaking = []
+    // for each hand in cards i
+    //   if i !== best-player
+    //   cardsForTheTaking.push(hand.shift());
+    // endfor
+    // cards[best-player].push(cardsForTheTaking);
+  }
+
+  renderUpCard(i) {
+    return (
+      <UpCard 
+        card={this.state.cards[i][0]} 
+        handleCategorySelection={this.handleCategorySelection} 
+      />
+    )
+  }
+
   render() {
     // TODO: Make multiplayer?
     // var numPlayers = 2;
     // for (var i=0; i < numPlayers; i++) {
     //   rows.push(<PlayersHand player="i" playersCards="this.props.servedCards"/>);
     // }
-
+    const firstPlayersTurn = (this.state.currPlayer === 0);
     return (
       <div className="Game">
-      	<PlayersHand player="1" isPlayersTurn={1} playersCards={SERVED_CARDS[0]}/>
-      	<PlayersHand player="2" isPlayersTurn={0} playersCards={SERVED_CARDS[1]}/>
+
+        <div className="players-hand players-hand--first-player">
+          <h2>Player 1:</h2>
+          { firstPlayersTurn? this.renderUpCard(0) : ( <DownCard /> )}
+        </div>
+
+        <div className="players-hand players-hand--second-player">
+          <h2>Player 2:</h2>
+          { firstPlayersTurn? ( <DownCard /> ) : this.renderUpCard(1) }
+        </div>
+
       </div>
     );
-  }
-}
-
-class PlayersHand extends Component {
-  render() {
-    if (this.props.isPlayersTurn) {
-      return <UpCard card={this.props.playersCards[0]} />
-    }
-    else {
-      return <DownCard />
-    }
   }
 }
 
@@ -48,7 +80,7 @@ class UpCard extends Component {
     for (var category in trumpRatings) {
       trumpRatingRows.push( <TrumpRatingRow category={category} rating={trumpRatings[category]} /> );
     }
-
+    debugger;
     return (
       <div className="trump-card">
         <h3 className="trump-card-header">{this.props.card.name}</h3>
