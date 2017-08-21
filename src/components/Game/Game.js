e862e7e66acd238226ac10fb660112549ca27d66
cWilgar <carolinewilgar@gmail.com>
2017-08-21 12:13:51 +0100

    Further refactor for handle click - need to pass category to handleClick from container silly

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -18,18 +18,26 @@ class Game extends Component {
     super(props);
     this.state = {
       cards: SERVED_CARDS, //the players current card is the one at index 0 of each hand
-      currPlayer: 0,
-      selectedCategory: '',
+      currPlayer: 0
     }
   };
 
   handleCategorySelection(category) {
+    //--checkForRoundWinner:
     // best-player = 0;
     // for each player i
     //   if category rank < player-1's rank
     //   bestplayer = i
     // endfor
 
+    const cards = this.state.cards;
+
+    let roundWinner = 0;
+    for (let i = 0; i < cards.length; i++) {
+      debugger;
+    }
+
+    //--handCardsToRoundWinner:
     // const cards = this.state.cards.slice();
     // let cardsForTheTaking = []
     // for each hand in cards i
@@ -37,13 +45,32 @@ class Game extends Component {
     //   cardsForTheTaking.push(hand.shift());
     // endfor
     // cards[best-player].push(cardsForTheTaking);
+
+    //--
+    //setState cards > cards
+    //setState currPlayer = currPlayer ? 0 : 1;
+
+    //--checkForGameWinner:
   }
 
   renderUpCard(i) {
+
+    const trumpRatings = this.props.card.ratings
+    var trumpRatingRows = []
+    for (var category in trumpRatings) {
+      trumpRatingRows.push( 
+        <TrumpRatingRow 
+          category={category} 
+          rating={trumpRatings[category]} 
+          handleClick={() => this.handleCategorySelection(category)}
+        /> 
+      );
+    }
+
     return (
-      <UpCard 
-        card={this.state.cards[i][0]} 
-        handleCategorySelection={this.handleCategorySelection} 
+      <UpCard
+        card={this.state.cards[i][0]}
+        trumpRatingRows={trumpRatingRows}
       />
     )
   }
@@ -71,35 +98,57 @@ class Game extends Component {
       </div>
     );
   }
+
+  renderUpCard(i) {
+    const card = this.state.cards[i][0];
+    return (
+      <UpCard
+        card={card}
+        trumpRatingRows={this.renderTrumpRatings(card.ratings)}
+      />
+    )
+  }
+
+  renderTrumpRatings(ratings) {
+    var trumpRatingRows = [];
+    for (var category in ratings) {
+      trumpRatingRows.push( 
+        <TrumpRatingRow 
+          category={category} 
+          rating={ratings[category]} 
+          handleClick={() => this.handleCategorySelection(category)}
+        /> 
+      );
+    }
+    return trumpRatingRows;
+  }
+
 }
 
 class UpCard extends Component {
   render() {
-    const trumpRatings = this.props.card.ratings
-    var trumpRatingRows = []
-    for (var category in trumpRatings) {
-      trumpRatingRows.push( <TrumpRatingRow category={category} rating={trumpRatings[category]} /> );
-    }
-    debugger;
     return (
       <div className="trump-card">
         <h3 className="trump-card-header">{this.props.card.name}</h3>
         <img src={this.props.card.picture} />
         <div className="trump-card-ratings">
           <ol>
-            {trumpRatingRows}
+            {this.props.trumpRatingRows}
           </ol>
         </div>
       </div>
     )
   }
 }
+
 class TrumpRatingRow extends Component {
   render() {
     return (
       <li>
-        <span>{this.props.category}: </span>
-        <span>{this.props.rating}</span>
+        <a onClick={this.props.handleClick}>
+          <span>{this.props.category}: </span>
+          <span>{this.props.rating}</span>
+        </a>
       </li>
     )
   }
